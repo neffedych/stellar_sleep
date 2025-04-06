21b2fcd7e24943e4ec859d0830bfcef62a695f7f
Artem Nefedov <artnefedov.dev@gmail.com>
2025-04-05 23:45:36 -0700

    feat: implemented models n serializers

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Provider(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        db_table = 'providers'
+
+class Patient(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.TextField()
+    middle_name = models.TextField(null=True, blank=True)
+    last_name = models.TextField()
+    date_of_birth = models.DateField()
+    status = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    provider_id = models.UUIDField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        db_table = 'patients'
+
+
+
+class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField()
+    first_address = models.TextField()
+    second_address = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_address}, {self.second_address}"
+
+    class Meta:
+        db_table = 'addresses'
+
+class CustomField(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    field_name = models.TextField()
+    field_type = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.field_name
+
+    class Meta:
+        db_table = 'custom_fields'
+
+
+class PatientCustomField(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient_id = models.UUIDField()
+    custom_field_id = models.UUIDField()
+    field_value = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Custom Field for Patient {self.patient_id}"
+
+    class Meta:
+        db_table = 'patient_custom_fields'
---
+++ backend/patients/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Provider, Patient, Address, CustomField, PatientCustomField
+
+class ProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ['id', 'first_name', 'last_name', 'email', 'created_at', 'updated_at']
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ['id', 'first_name', 'middle_name', 'last_name', 'date_of_birth', 'status', 'created_at', 'updated_at', 'provider_id']
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['id', 'user_id', 'first_address', 'second_address', 'created_at', 'updated_at']
+
+class CustomFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomField
+        fields = ['id', 'field_name', 'field_type', 'created_at', 'updated_at']
+
+class PatientCustomFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientCustomField
+        fields = ['id', 'patient_id', 'custom_field_id', 'field_value', 'created_at', 'updated_at']
