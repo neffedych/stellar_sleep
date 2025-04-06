91db71f6b67027690f5d5dfe5686c60d34470742
Artem Nefedov <artnefedov.dev@gmail.com>
2025-04-05 22:57:12 -0700

    chore: db setup

--- backend/dashboard/settings.py
+++ backend/dashboard/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +76,17 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'test',
+        'HOST': 'db.ryrhvmpgbiylizttfqqq.supabase.co', 
+        'PORT': '5432',  
     }
 }
+
+
+
 
 
 # Password validation
